d9248690833e3bc4072ac743339176e1652db265
Eudis Duran <eudisd@gmail.com>
2016-11-07 20:51:16 -0500

    more action creator tests

--- test/unit/reducers/accounts.spec.js
+++ test/unit/reducers/accounts.spec.js
@@ -13,4 +13,37 @@ describe('accounts reducer', () => {
       response: 'My Test State'
     })).to.deep.equal({ internal: 'My Test State' });
   });
+
+  it('sets the first internal account on SET_SELECTED_INTERNAL_ACCOUNT error', () => {
+    expect(accounts({
+    }, {
+      type: accountTypes.SET_SELECTED_INTERNAL_ACCOUNT,
+      id: 2
+    })).to.deep.equal({
+      selectedInternalAccount: undefined
+    })
+  });
+
+  it('sets the selected internal account on SET_SELECTED_INTERNAL_ACCOUNT', () => {
+    expect(accounts({
+      internal: [{id: 1}]
+    }, {
+      type: accountTypes.SET_SELECTED_INTERNAL_ACCOUNT,
+      id: 1
+    })).to.deep.equal({
+      internal: [{id: 1}],
+      selectedInternalAccount: {id: 1}
+    })
+  });
+
+  it('returns the selected internal account on GET_SELECTED_INTERNAL_ACCOUNT', () => {
+    expect(
+    accounts({
+      selectedInternalAccount: {id: 1}
+    }, {
+      type: accountTypes.GET_SELECTED_INTERNAL_ACCOUNT,
+    })).to.deep.equal({
+      id: 1
+    });
+  });
 });
